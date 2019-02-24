3aaf4e7ae874fe782044f2efe466ce28392e9842
Naruki Yoshikawa <naruki.yoshikawa@gmail.com>
2019-02-24 06:44:19 +0000

    Update RDKit convert script

--- rdkit/convert-rdkit.py
+++ rdkit/convert-rdkit.py
@@ -6,8 +6,9 @@ w = Chem.SDWriter('platinum-rdkit-etkdg.sdf')
 
 with open("../data/platinum.smi", "r") as f:
     for line in f:
-        smiles = line.split()[0]
+        smiles, entry = line.split()
         mol = Chem.MolFromSmiles(smiles)
+        mol.SetProp("_Name", entry)
         mol = Chem.AddHs(mol)
         AllChem.EmbedMolecule(mol, AllChem.ETKDG())
         w.write(mol)
