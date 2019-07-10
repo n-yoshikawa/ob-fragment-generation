7cb6c4e372b4fef66f03f5bc6615d2d6947997ee
Naruki Yoshikawa <naruki.yoshikawa@gmail.com>
2019-07-10 17:22:05 +0900

    Add files for revision

--- evaluation/mwplot.py
+++ evaluation/mwplot.py
@@ -5,12 +5,14 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 sns.set(style="white")
+sns.set_context("paper", 1.5)
 
 df = pd.read_csv(sys.argv[1])
 
-plt.plot(df['MW'], df['RMSD'], '.')
-plt.xlabel("Molecular weight (Da)")
+h = sns.jointplot(df['MW'], df['RMSD'], kind="scatter", marker='.')
+h.ax_joint.set_xlabel("Molecular weight (Da)")
+h.ax_joint.set_ylabel("RMSD ($\AA$)")
 plt.xlim([100, 650])
-plt.ylabel("RMSD ($\AA$)")
 plt.ylim([0, 7])
-plt.show()
+plt.tight_layout()
+plt.savefig("mw-rdkit.pdf")
